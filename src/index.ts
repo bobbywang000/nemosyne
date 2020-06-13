ad219d9206f52534a5133397134f292a48bb1df4
Bobby Wang <bobbywang000@gmail.com>
2020-06-12 19:36:17 -0700

    Add error handling for bad requests (inseaad of just timing out)

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,27 @@ createConnection()
         Routes.forEach((route) => {
             // eslint-disable-next-line @typescript-eslint/ban-types
             (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-                const result = new (route.controller as any)()[route.action](req, res, next);
+                const result = new (route.controller as any)()[route.action](req, res, next).catch((error) => {
+                    console.log(error);
+                    res.status(404).send(res.render('error', { error: JSON.stringify(error, null, 2) }));
+                });
                 if (result instanceof Promise) {
                     result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
                 } else if (result !== null && result !== undefined) {
                     res.json(result);
                 }
             });
+        });
+
+        //The 404 Route (ALWAYS Keep this as the last route)
+        app.all('*', (req, res, next) => {
+            res.status(404).send(
+                `Error:
+                    Body:${JSON.stringify(req.body)}
+                    Params:${JSON.stringify(req.params)}
+                    Query Params:${JSON.stringify(req.query)}
+                `,
+            );
         });
 
         // setup express app
