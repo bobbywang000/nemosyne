259ec2bb97c316395f85bc440ea904899241e26a
Bobby Wang <bobbywang000@gmail.com>
2021-01-16 18:59:57 -0800

    Change db backup script to output files with alphabetical sorted names

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -3,5 +3,5 @@
 # open database, wait up to 20 seconds for any activity to end and create a backup file
 sqlite3 "$1" <<EOF
 .timeout 20000
-.backup "${OUTPUT_FOLDER:-.}/$(date)-$1.bak"
+.backup "${OUTPUT_FOLDER:-.}/$1-$(date -u +"%Y-%m-%dT%H_%M_%SZ").bak"
 EOF
